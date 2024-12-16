b148bf05fa1eef1d367c4f76df184c797a56b3e2
vishnuparihar <vishnuparihar239925@gmail.com>
2024-12-16 22:04:08 +0530

    react day02

--- DAY01/src/App.jsx
+++ DAY01/src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div class=" bg-indigo-600 text-lg font- " >app</div>
+        <navbar  > <ul className=" h-20 bg-blue-800 font-medium flex justify-center items-center text-3xl font-bold font-black gap-4">
+            <li >Home</li>
+            <li>Menu</li>
+            <li>Contact</li>
+            <li>About</li>
+        </ul> </navbar>
+
+       
     )
 }
 
