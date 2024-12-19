f2e7e1280d8bd272f42c17a0d6df5f0c9b833331
vishnuparihar <vishnuparihar239925@gmail.com>
2024-12-19 21:59:57 +0530

    DAY04

---
+++ DAY04/src/utility/script.js
@@ -0,0 +1,9 @@
+
+import products from "./data.js";
+console.log(products);
+// products.map((product) => console.log(product));
+const p = products.map((product) => ({
+  category: product.category,
+  item: product.productName,
+}));
+console.log(p);
