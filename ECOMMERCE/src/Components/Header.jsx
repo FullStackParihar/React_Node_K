815c3108752f3f64387dc873c7fc5787acd31de6
vishnuparihar <vishnuparihar239925@gmail.com>
2024-12-30 21:31:14 +0530

    project

--- ECOMMERCE/src/Components/Header.jsx
+++ ECOMMERCE/src/Components/Header.jsx
@@ -8,29 +8,30 @@ const Header = () => {
       </div>
 
       <nav>
-        <ul className="flex gap-6">
-          <li className="bg-white text-purple-500 px-4 py-2 rounded-full shadow-md hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
+        <ul className="flex gap-5">
+          <li className=" text-white px-4 py-2 hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
             Home
           </li>
-          <li className="bg-white text-purple-500 px-4 py-2 rounded-full shadow-md hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
+          <li className=" text-white px-4 py-2 hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
             Menu
           </li>
-          <li className="bg-white text-purple-500 px-5 py-2 rounded-full shadow-md hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
+          <li className=" text-white px-5 py-2 hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
             My Account
           </li>
-          <li className="bg-white text-purple-500 px-4 py-2 rounded-full shadow-md hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
+          <li className=" text-white px-4 py-2 hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
             Settings
           </li>
-          <li className="bg-white text-purple-500 px-4 py-2 rounded-full shadow-md hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
+          <li className=" text-white px-4 py-2 hover:scale-105 hover:shadow-2xl hover:bg-purple-700 hover:text-white transition-all duration-300 cursor-pointer ">
             About
           </li>
         </ul>
       </nav>
 
 
-      <div className="flex items-center gap-3 text-white text-2xl cursor-pointer">
-        <span className="px-6 flex gap-2 material-icons bg-teal-500 p-2 rounded-full shadow-md hover:bg-teal-700 transition-all duration-300">
-         <img className="h-8" src="https://cdn-icons-png.flaticon.com/128/54/54481.png" alt="" /> search
+      <div className="flex items-center gap-3 opacity-70 text-white text-2xl cursor-pointer">
+        <span className="px-10 flex justify-start gap-2 material-icons bg-teal-500 p-2 rounded-full shadow-md hover:bg-teal-700 transition-all duration-300">
+         <img className="h-8" src="https://cdn-icons-png.flaticon.com/128/54/54481.png" alt="" />
+           search  
         </span>
 
       </div>
