d587b2ec0089524aa9af179d698ea1ef1daf3ecc
vishnuparihar <vishnuparihar239925@gmail.com>
2024-12-30 18:29:31 +0530

    project

--- ECOMMERCE/src/Components/Main.jsx
+++ ECOMMERCE/src/Components/Main.jsx
@@ -3,6 +3,11 @@ import products from "../Utility/data"
 const Main = () => {
   return (
     <>
+    <div>
+      <div>
+      <img className=" h-fit w-full " src="https://www.shutterstock.com/shutterstock/photos/1364474708/display_1500/stock-vector-creative-vector-illustration-of-big-sale-banner-with-phone-isolated-on-transparent-background-art-1364474708.jpg" alt="" /></div>
+      <div>manu bar</div>
+    </div>
       <div className="flex flex-wrap justify-center items-center min-h-screen bg-gray-100 p-0">
         <h1 className="text-4xl font-bold text-gray-800 mb-2">Products</h1>
         <CardComponent contacts={products} />
