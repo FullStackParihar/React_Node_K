e71496b65d154a837e93ba6f81fe15fcf2bd6a6e
vishnuparihar <vishnuparihar239925@gmail.com>
2025-01-07 23:40:26 +0530

    ecommerce

---
+++ DAY08/src/utility/data.js
@@ -0,0 +1,25 @@
+const categories = [
+    {
+        id: 1,
+        name: 'Apple',
+        
+    },
+    {
+        id: 2,
+        name: 'Banana',
+         
+    },
+    {
+        id: 3,
+        name: 'Orange',
+        
+    },
+    {
+        id: 4,
+        name: 'Mango',
+         
+    },
+     
+];
+
+export default categories;
