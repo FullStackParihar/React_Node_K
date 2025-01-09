7fd38869c18f742ff07df0b11f29cc144094457b
vishnuparihar <vishnuparihar239925@gmail.com>
2025-01-10 00:14:41 +0530

    classroom

--- ClassRoom/src/App.jsx
+++ ClassRoom/src/App.jsx
@@ -5,16 +5,17 @@ import NavBar from "./Components/NavBar";
 import SideBar from "./Components/SideBar";
 const App = () => {
   return (
-    <div>
-    
-      <NavBar />
-
-       <SideBar className="border-2" />
-      <div className="flex flex-wrap gap-6 p-8 justify-start">
-      {products[0].map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <div className="relative">
+      <div className="fixed top-0 w-[100%] z-10 bg-white">
+        <NavBar /></div>
+      <div className="fixed top-20 p-8 border-x-2 h-[100%] ">
+        <SideBar />
+      </div>
+      <div className="flex flex-wrap gap-6 mt-28 ml-32 justify-start">
+        {products[0].map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
--- ClassRoom/src/Components/SideBar.jsx
+++ ClassRoom/src/Components/SideBar.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
- 
+import { faBoxArchive } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 const SideBar = () => {
     return (
 
-        <div>
+        <div className='flex flex-col gap-10 ' >
             <div> <FontAwesomeIcon icon={faHouse} /></div>
             <div><FontAwesomeIcon icon={faGear} />
             </div>
- 
+            <div><FontAwesomeIcon icon={faCalendar} />
+            </div>
+            <div><FontAwesomeIcon icon={faGraduationCap} />
+            </div>
+            <div><FontAwesomeIcon icon={faBoxArchive} />
+            </div>
         </div>
 
     )
